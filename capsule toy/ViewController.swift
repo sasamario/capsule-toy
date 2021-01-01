f0ba3514a1566c2bebadafc749af3937df80a3d2
sasama <mametaku3@yahoo.co.jp>
2021-01-01 17:20:24 +0900

    Add：プロジェクトを追加

---
+++ capsule toy/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  capsule toy
+//
+//  Created by 笹間巧馬 on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
