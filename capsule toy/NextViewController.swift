103ef5feca92dbc9e4798dbcd1cad9fbaf20379b
sasama <mametaku3@yahoo.co.jp>
2021-01-07 18:17:31 +0900

    Add：NextViewControllerを追加 Add：配列ないからランダムで一つを表示させ、デバイスに保存する処理を追加

---
+++ capsule toy/NextViewController.swift
@@ -0,0 +1,56 @@
+//
+//  NextViewController.swift
+//  capsule toy
+//
+//  Created by 笹間巧馬 on 2021/01/03.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    let messageArray = [
+        "今日も1日お疲れ様です！早く寝ましょう。",
+        "ナイスファイト！最高！",
+        "明日はゆっくり休みましょう",
+        "あいうえおおおおおおおおお",
+        "ナイス！"
+    ]
+    
+    @IBOutlet weak var message: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //指定範囲内でランダムな整数を生成
+        let randomInt = Int.random(in: 0..<5)
+        
+        message.text = messageArray[randomInt]
+        
+        //もしデバイス内にメッセージが保存されていなかったら保存する
+        if UserDefaults.standard.object(forKey: "\(randomInt)") == nil {
+            UserDefaults.standard.set(messageArray[randomInt], forKey: "\(randomInt)")
+        }
+        
+    }
+    
+    
+    @IBAction func back(_ sender: Any) {
+        
+        //前の画面に戻る
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
